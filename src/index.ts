c122a51881222c9093b1c647fd4a56be4c4fd846
irishcarbomb777 <danielclannan@gmail.com>
2023-08-15 17:21:33 -0400

    mod

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './hooks/useCallbackResult.js'
 export * from './components/DependencyLayer/DependencyLayer.js'
+export * from './components/DependencyLayer/hooks/useDependencyResolver.js'
